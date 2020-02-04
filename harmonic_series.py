45c6f7531b94c7acf5ea42b8c190caf81011d41a
missing-user <philipp.jurasic@gmx.de>
2020-02-04 07:56:17 +0100

    working  script

--- harmonic_series.py
+++ harmonic_series.py
@@ -1,36 +1,48 @@
 import colorsys
 import cv2
-import numpy as no
+import numpy as np
 
 hue = 0
-color = colorsys.hls_to_rgb(hue, 0.5, 0.8)
+color = (colorsys.hls_to_rgb(hue, 128, 200))
 
+window = 1000
+towerHeight = 50
 width = 30
-height = 5
+height = float(window)/towerHeight
 xStart = 10
 
-img = np.zeros((512,512,3), np.uint8)
+img = np.zeros((window,window,3), np.uint8)
 
 # draw the vertical start line
-img = cv2.line(img,(xStart,0),(xStart,511),color,1)
-img = cv2.line(img,(xStart+width,0),(xStart+width,511),color,1)
+img = cv2.line(img,(xStart+width,0),(xStart+width,window-1),color,1)
+img = cv2.line(img,(xStart+2*width,0),(xStart+2*width,window-1),color,1)
 
-offset = 0
+offset = 0.0
+hue = 0.1
 
-for x in range(20):
-  offset += 1/x
-  print(offset)
+for x in range(1,towerHeight):
+      offset += 1/(x)
+      print()
+      print(offset)
 
-  # set new rectangle color
-  hue += 0.05
-  hue = hue % 1
-  color = colorsys.hls_to_rgb(hue, 0.5, 0.8)
+      # set new rectangle color
+      hue += 1.0/towerHeight
+      color = (colorsys.hls_to_rgb(hue, 128, 200))
+
+      xpos = xStart + offset * width
+      startPoint = (int(xpos),int(x*height))
+      endPoint = (int(xpos+width),int((x+1)*height))
+      print('start',startPoint)
+      print('end',endPoint)
+      # draw the actual rect
+      img = cv2.rectangle(img,startPoint,endPoint,color,1)
 
 
-  xpos = xStart + offset * width
-  # draw the actual rect
-  cv2.rectangle(img,(xpos,x*height),(xpos+width,(x+1)*height),color,2)
-  
+# draw the vertical end line
+endLinex = int(xStart + offset * width)
+img = cv2.line(img,(endLinex,0),(endLinex,window-1),color,1)
+img = cv2.line(img,(endLinex+width,0),(endLinex+width,window-1),color,1)
+
 
 cv2.imshow('image',img)
 cv2.waitKey(0)
