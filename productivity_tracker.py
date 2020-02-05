76121e1849e89b684eb30209f765e0a8fd6577eb
missing-user <philipp.jurasic@gmx.de>
2020-02-05 21:34:49 +0100

    working productivity tracker

--- productivity_tracker.py
+++ productivity_tracker.py
@@ -1,20 +1,43 @@
+from win32gui import GetForegroundWindow
+import psutil
+import time
+import win32process
 import matplotlib.pyplot as plt
 import numpy as np
 
-task_names = ['WRITING PRODUCTIVITY\nTRACKER', 'TIME SPENT\n PRODUCTIVE']
-task_times = [12000, 4555]
+process_time={}
+timestamp = {}
+
 
 plt.xkcd()
 
-fig, ax = plt.subplots()
+def draw_chart(process_dict):
 
-print(len(task_names))
-plt.bar(np.arange(len(task_names)), task_times)
-plt.xticks(task_times, task_names)
+    tasks = ['WRITING PRODUCTIVITY\nTRACKER']
+    times = [600]
+    for i in process_dict:
+        tasks.append(i)
+        times.append(process_dict[i])
 
-plt.title("PRODUCTIVITY SO FAR")
 
-plt.draw()
-plt.show()
-# plt.pause(10)
-# plt.clf()
+    y_pos = np.arange(len(tasks))
+
+    plt.clf()
+    plt.bar(y_pos, times, align='center')
+    plt.xticks(y_pos, tasks)
+    plt.ylabel('USAGE TIME')
+
+    plt.title("PRODUCTIVITY SO FAR")
+    plt.pause(1)
+
+
+
+while True:
+    current_app = psutil.Process(win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]).name().replace(".exe", "")
+    timestamp[current_app] = int(time.time())
+    time.sleep(1)
+    if current_app not in process_time.keys():
+        process_time[current_app] = 0
+    process_time[current_app] = process_time[current_app]+int(time.time())-timestamp[current_app]
+    print(process_time)
+    draw_chart(process_time)
