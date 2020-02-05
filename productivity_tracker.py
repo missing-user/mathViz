74d519de873752344cbec2f4d1cb99d5928c27a2
missing-user <philipp.jurasic@gmx.de>
2020-02-05 23:04:33 +0100

    fixed some bugs, now its an actually usable tracker

--- productivity_tracker.py
+++ productivity_tracker.py
@@ -1,43 +1,58 @@
 from win32gui import GetForegroundWindow
 import psutil
+import matplotlib.animation as animation
 import time
 import win32process
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
-process_time={}
+process_time = {}
 timestamp = {}
 
 
 plt.xkcd()
+fig, ax = plt.subplots()
 
-def draw_chart(process_dict):
-
+def draw_chart(frame):
+    process_dict = get_process_times()
     tasks = ['WRITING PRODUCTIVITY\nTRACKER']
     times = [600]
     for i in process_dict:
-        tasks.append(i)
-        times.append(process_dict[i])
+        if(process_dict[i] > 20):
+            tasks.append(i.upper())
+            times.append(process_dict[i])
+
+    y_pos = np.arange(len(times))
 
 
+    ax.clear()
+    plt.xkcd()
+    ax.bar(y_pos, times, align='center')
     y_pos = np.arange(len(tasks))
-
-    plt.clf()
-    plt.bar(y_pos, times, align='center')
+    ax.set_ylabel('TIME SPENT')
+    ax.set_title("PRODUCTIVITY SO FAR")
     plt.xticks(y_pos, tasks)
-    plt.ylabel('USAGE TIME')
-
-    plt.title("PRODUCTIVITY SO FAR")
-    plt.pause(1)
 
 
+anim_interval = 1
+process_time={}
 
-while True:
-    current_app = psutil.Process(win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]).name().replace(".exe", "")
-    timestamp[current_app] = int(time.time())
-    time.sleep(1)
+
+def get_process_times():
+    try:
+        current_app = psutil.Process(win32process.GetWindowThreadProcessId(GetForegroundWindow())[1]).name().replace(".exe", "")
+    except:
+        print('error getting process')
+        return process_time
+
     if current_app not in process_time.keys():
         process_time[current_app] = 0
-    process_time[current_app] = process_time[current_app]+int(time.time())-timestamp[current_app]
+    process_time[current_app] += anim_interval
     print(process_time)
-    draw_chart(process_time)
+    return process_time
+
+
+animator = animation.FuncAnimation(fig, draw_chart, interval=500)
+
+plt.show()
